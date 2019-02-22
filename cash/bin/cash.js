92d858dc7a99698b9809266dc33f36e0f426ddd2
benoit delattre <delattreben@gmail.com>
2019-02-22 20:50:04 +0100

    docs(readme): add usage section

--- cash/bin/cash.js
+++ cash/bin/cash.js
@@ -1,18 +1,26 @@
 'use strict';
 
+// Importer les librairie
 const got = require('got');
 const money = require('money');
 const chalk = require('chalk');
 const ora = require('ora');
 const currencies = require('../lib/currencies.json');
 
+
+//recupere l url du court actuel de la monnaie dans le fichier constants.js
 const {API} = require('./constants');
 
+//fonction utilise dans index.js 
 const cash = async command => {
-	const {amount} = command;
+	const {amount} = command
+	//Récupérer et utiliser les attributs de command pour la conversion 
+	
+	const amount = command.amount;
 	const from = command.from.toUpperCase();
 	const to = command.to.filter(item => item !== from).map(item => item.toUpperCase());
 
+	//apparence des resultat dans la console
 	console.log();
 	const loading = ora({
 		text: 'Converting...',
@@ -25,20 +33,25 @@ const cash = async command => {
 
 	loading.start();
 
+	//recupere les donné de l url
 	await got(API, {
 		json: true
 	}).then(response => {
+		
 		money.base = response.body.base;
 		money.rates = response.body.rates;
 
+		//Affiche pour chaque item la maonnaie convertie en une autre avec 3 decimal
 		to.forEach(item => {
 			if (currencies[item]) {
 				loading.succeed(`${chalk.green(money.convert(amount, {from, to: item}).toFixed(3))} ${`(${item})`} ${currencies[item]}`);
 			} else {
+				//erreur
 				loading.warn(`${chalk.yellow(`The "${item}" currency not found `)}`);
 			}
 		});
 
+		//erreur
 		console.log(chalk.underline.gray(`\nConversion of ${chalk.bold(from)} ${chalk.bold(amount)}`));
 	}).catch(error => {
 		if (error.code === 'ENOTFOUND') {
@@ -49,5 +62,6 @@ const cash = async command => {
 		process.exit(1);
 	});
 };
+
 
 module.exports = cash;
--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -7,25 +7,32 @@ describe('starwars-names', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
     test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      for(var i=0; i < starWarsNames.length; i++)
+      {
+         expect(typeof "SWNames[i]").toBe("string");
+      }
+     
     });
 
     test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+    
+      expect(starWarsNames).toEqual(expect.arrayContaining(["Luke Skywalker"]));
+
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+    expect(starWars.all).not.toContain("Ben Quadinaros");
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+       expect(starWars.all).toContain(starWars.random());
     });
 
     test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      var i = Math.floor((Math.random() * starWars.length)+1);
+     expect(starWars.all).toEqual(expect.arrayContaining(starWars.random(i)));     
     });
   });
 });
